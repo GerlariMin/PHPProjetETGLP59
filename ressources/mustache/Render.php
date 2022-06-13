ff78aa4deea4e55aae06a9e185eec31f40e51978
Morgan MINBIELLE <hornitzaile@hotmail.fr>
2022-06-13 11:56:09 +0200

    Ajout des ressources communes
    Ajout des fichiers communs utiles pour le développement.

---
+++ ressources/mustache/Render.php
@@ -0,0 +1,54 @@
+<?php
+
+    /**
+     * Class Render
+     */
+    class Render
+    {
+        /**
+         * @var Mustache_Engine
+         */
+        private Mustache_Engine $mustache;
+
+        /**
+         * Render constructor.
+         * @param string $chemin
+         */
+        public function __construct(string $chemin = "")
+        {
+            $this->mustache = new Mustache_Engine(
+                [
+                    'loader' => new Mustache_Loader_CascadingLoader(
+                        [
+                            new Mustache_Loader_FilesystemLoader($chemin . 'ressources/mustache'),
+                            new Mustache_Loader_FilesystemLoader($chemin . 'accueil/mustache'),
+                            new Mustache_Loader_FilesystemLoader($chemin . 'cgu/mustache'),
+                            new Mustache_Loader_FilesystemLoader($chemin . 'connexion/mustache'),
+                            new Mustache_Loader_FilesystemLoader($chemin . 'erreur/mustache'),
+                            new Mustache_Loader_FilesystemLoader($chemin . 'inscription/mustache'),
+                            new Mustache_Loader_FilesystemLoader($chemin . 'profil/mustache'),
+                            new Mustache_Loader_FilesystemLoader($chemin . 'tableau-de-bord/mustache'),
+                        ]
+                    )
+                ]
+            );
+        }
+
+        /**
+         * Fonction permettant d'afficher le contenu Mustache d'une page, avec les données contenues dans la variable $data
+         * @param array $data
+         */
+        public function actionRendu(array $data = [])
+        {
+            //On extrait les données à afficher
+            extract($data, EXTR_OVERWRITE);
+            if(isset($_SESSION['login'])) {
+                $data['connecte'] = true;
+            }
+            //On affiche les données voulues
+            echo $this->mustache->render("Body", $data);
+        }
+
+    }
+
+?>
